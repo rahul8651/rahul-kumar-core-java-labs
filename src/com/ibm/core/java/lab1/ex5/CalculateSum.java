f912cfda55d76a3c9a008e3b656bef76b92e6834
Rahul Kumar <rahul.kumar07@ibm.com>
2026-06-04 16:57:42 +0530

    Updated

--- src/com/ibm/core/java/lab1/ex5/CalculateSum.java
+++ src/com/ibm/core/java/lab1/ex5/CalculateSum.java
@@ -1,5 +1,26 @@
 package com.ibm.core.java.lab1.ex5;
 
+import java.util.Scanner;
+
 public class CalculateSum {
+
+	static int calculateSum(int n) {
+		int sum = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		calculateSum(n);
+		sc.close();
+	}
 
 }
--- src/com/ibm/core/java/lab1/ex6/SquareDifference.java
+++ src/com/ibm/core/java/lab1/ex6/SquareDifference.java
@@ -1,5 +1,35 @@
 package com.ibm.core.java.lab1.ex6;
 
+import java.util.Scanner;
+
 public class SquareDifference {
 
+	static int calculateDifference(int n) {
+
+		int sumOfSquares = 0;
+		int sumOfNumbers = 0;
+
+		for (int i = 1; i <= n; i++) {
+			sumOfSquares += i * i;
+			sumOfNumbers += i;
+		}
+
+		int squareOfSum = sumOfNumbers * sumOfNumbers;
+
+		return squareOfSum - sumOfSquares;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the Integer Value: ");
+		int n = sc.nextInt();
+
+		int result = calculateDifference(n);
+
+		System.out.println("Difference = " + result);
+
+		sc.close();
+	}
 }
